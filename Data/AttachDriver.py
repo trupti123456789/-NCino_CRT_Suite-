9d24d534c230291078dbf089f640f24b9c3759a0
Trupti  Shantaram Chaudhary <trupti.c@cloudfulcrum.com>
2026-01-27 08:48:19 +0000

    Deleting AttachDriver.py file via Copado QEditor.

--- Data/AttachDriver.py
+++
@@ -1,8 +0,0 @@
-# file: attach_selenium.py
-from robot.libraries.BuiltIn import BuiltIn
-from SeleniumLibrary import SeleniumLibrary
-
-def attach_driver():
-    qweb_driver = BuiltIn().get_library_instance("QWeb").driver
-    selib = BuiltIn().get_library_instance("SeleniumLibrary")
-    selib.register_driver(qweb_driver, "default")
